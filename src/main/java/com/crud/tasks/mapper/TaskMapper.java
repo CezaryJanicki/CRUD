8f571384118e52d548a937ccbcb698b60ce80415
CezaryJanicki <cezary.janicki@gmail.com>
2020-11-19 08:24:48 +0100

    for comparison template classes

--- src/main/java/com/crud/tasks/mapper/TaskMapper.java
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -5,29 +5,18 @@ import com.crud.tasks.domain.TaskDto;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class TaskMapper {
     public Task mapToTask(final TaskDto taskDto) {
-        return new Task(
-                taskDto.getId(),
-                taskDto.getTitle(),
-                taskDto.getContent()
-        );
+        return null;
     }
 
     public TaskDto mapToTaskDto(final Task task) {
-        return new TaskDto(
-                task.getId(),
-                task.getTitle(),
-                task.getContent()
-        );
+        return null;
     }
 
     public List<TaskDto> mapToTaskDtoList(final List<Task> taskList) {
-        return taskList.stream()
-                .map(t -> new TaskDto(t.getId(), t.getTitle(), t.getContent()))
-                .collect(Collectors.toList());
+        return null;
     }
 }
