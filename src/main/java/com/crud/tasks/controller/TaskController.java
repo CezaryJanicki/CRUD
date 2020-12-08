0710d6382de9d7fcfa4d1518cf41aa0a17f23c3a
CezaryJanicki <cezary.janicki@gmail.com>
2020-12-08 23:07:55 +0100

    after code review

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,27 +29,29 @@ public class TaskController {
     }
 
     @GetMapping("/getTask")
-    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+    public TaskDto getTask(@RequestParam Long taskId) {
         return taskMapper.mapToTaskDto(dbService.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
     @DeleteMapping("/deleteTask")
-    public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException {
-        if (dbService.getTask(taskId).isPresent()) {
+    public void deleteTask(@RequestParam Long taskId) {
+        try {
             dbService.deleteTask(taskId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new TaskNotFoundException();
+        }
+    }
+
+    @PutMapping(value = "/updateTask", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        if (dbService.getTask(taskDto.getId()).isPresent()) {
+            return taskMapper.mapToTaskDto(dbService.saveTask(taskMapper.mapToTask(taskDto)));
         } else {
             throw new TaskNotFoundException();
         }
     }
 
-    @PutMapping("/updateTask")
-    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
-        return taskMapper.mapToTaskDto(dbService.saveTask(taskMapper.mapToTask(taskDto)));
-    }
-
-    @PostMapping("/createTask")
-    @RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/createTask", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         dbService.saveTask(taskMapper.mapToTask(taskDto));
     }
